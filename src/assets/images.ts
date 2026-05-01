593fb0f8f9e6b0076893cfa1f93eff439ad494a9
abdouma22223 <tociba23@gmail.com>
2026-05-01 09:42:09 +0100

    assets: placeholder for images (to be filled by specialized scripts)

---
+++ src/assets/images.ts
@@ -0,0 +1,2 @@
+
+export const UBMA_LOGO = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJYAAABkCAYAAABtsBmKqCtwYQnYXwhDNqnyNhi1wFHx0SsDQbH_W_jMzFMHcEcUKmhqYKbOK25rAjyPF2KRycL8kTZQCOvunBQAw393v1yJ7Z30F34QfsjIs9_Im_lmA2xGG_ksGSas1vHDjY17cc0i9yc9LoIZ9LBNBEhaO38rcmc="; // Truncated for this example, but I will put the real full ones below in a script to write them properly.
